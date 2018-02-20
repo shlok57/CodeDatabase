c41ea750c08ee4f7bc33ece39fa83cc25746e2bc
shlok <shlok@ccs.neu.edu>
2018-02-20 18:15:13 -0500

    Updated MinCost
    Signed-off-by: shlok <shlok@ccs.neu.edu>

--- Dynamic Programming/MinCost.py
+++ Dynamic Programming/MinCost.py
@@ -2,17 +2,14 @@ def Prof():
 	C[0] = 0
 	for i in range(1, N):
 		mini = 99999
-		# minnode =N
 		for j in range(i):
-			# print i,j
-			print T[N-i-1]
-			if mini > (C[j] + T[N-i-1][N-j-1]):
-				mini = (C[j] + T[N-i-1][N-j-1])
+			if mini > (C[j] + T[i][j]):
+				mini = (C[j] + T[i][j])
 				minnode = j
-		print
 		C[i] = mini
 		S[i] = minnode
 		
+	# print C
 	print "Total Distance: " + str(C[N-1])
 	print_path()
 	# print S
@@ -24,7 +21,7 @@ def print_path():
 		n = S[n]
 	print Names[N-1]
 		
-T = [[0,5,10,12], [0,0,3,2], [0,0,0,4], [0,0,0,0]]
+T = [[0,0,0,0] , [4,0,0,0] , [6,3,0,0] , [12,10,5,0]]
 N = len(T)
 Names = ['A', 'B', 'C', 'D']
 C = [0 for i in range(N)]
