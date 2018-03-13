0b417a0b3037b6a780b613982c84d61051152e66
shlok <shlok@ccs.neu.edu>
2018-03-13 14:04:19 -0400

    HashTable done
    Signed-off-by: shlok <shlok@ccs.neu.edu>

--- DataStructures/Hash.py
+++ DataStructures/Hash.py
@@ -1,3 +1,5 @@
+import string
+
 class HashTable:
 
 	def __init__(self):
@@ -9,7 +11,16 @@ class HashTable:
 			result = (result * 131 + ord(c)) % 10000
 		return result
 
-	def insert(self, key, value = -1):
+	def find(self, key):
+		key = key.translate(None, string.punctuation).lower()
+		key_node = self.search(key)
+		if key_node:
+			return key_node.count
+		else:
+			return -1
+
+	def search(self, key):
+		# key = key.translate(None, string.punctuation).lower()
 		key_hash = self.get_hash(key)
 		hash_node = self.hash_array[key_hash]
 
@@ -20,68 +31,98 @@ class HashTable:
 				key_node = hash_node
 				break
 			hash_node = hash_node.next
+		return key_node
+
+	def insert(self, key, value = -1, pos = -1):
+		key = key.translate(None, string.punctuation).lower()
+		key_node = self.search(key)
+		key_hash = self.get_hash(key)
 		if key_node:
 			if value == -1:
-				key_node.increase_count()
+				key_node.increase_count(pos)
 			else:
 				key_node.count = value
 		else:
 			temp_next = self.hash_array[key_hash].next
-			self.hash_array[key_hash].next = HashNode(key)
+			self.hash_array[key_hash].next = HashNode(key, pos)
 			self.hash_array[key_hash].next.next = temp_next
 
 	def delete(self,key):
+		key = key.translate(None, string.punctuation).lower()
 		key_hash = self.get_hash(key)
 		hash_node = self.hash_array[key_hash]
 
-		key_node = None
-		next_node = hash_node.next
+		prev_node = hash_node
+		main_node = hash_node.next
 		# search if exists
-		while next_node:
-			if next_node.word == key:
+		while main_node:
+			if main_node.word == key:
 				break
-			key_node = next_node
-			next_node = next_node.next
-		if next_node:
-			key_node = next_node.next		
+			prev_node = main_node
+			main_node = main_node.next
+		if main_node:
+			prev_node.next = main_node.next		
 		else:
 			print "Key Error"
 	
+	def increase(self, key):
+		key = key.translate(None, string.punctuation).lower()
+		key_node = self.search(key)
+		if key_node:
+			key_node.increase_count()
+		else:
+			print "Key Error"		
+
+	def list_keys(self):
+		key_list = []
+		for i in range(10000):
+			temp = self.hash_array[i].next
+			if temp != None:
+				while temp:
+					key_list.append(temp.word)
+					temp = temp.next
+		return key_list
+
 	def print_hash(self):
 		for i in range(10000):
 			temp = self.hash_array[i].next
 			if temp != None:
 				while temp:
-					print "|| ", temp.word, "|" , str(temp.count), "|| -> ",
+					print "||", temp.word, ">" , str(temp.count), ">", str(temp.positions), "||\t\t",
 					temp = temp.next
 				print
-			
 
 
 class HashNode:
 
-	def __init__(self, word):
+	def __init__(self, word, pos = -1):
 		self.word = word
 		self.count = 1
 		self.next = None
+		self.positions = []
+		if pos != -1:
+			self.positions.append(pos)
 
-	def increase_count(self):
+	def increase_count(self, pos = -1):
 		self.count += 1
+		if pos != -1:
+			self.positions.append(pos)
 
 H = HashTable()
-A = "Output the list of words together with their counts on an output file.\
-	 For this problem,you cannot use built-in-language datastuctures that \
-	 can index by strings (like hashtables). Use a language that easily \
-	 implements linked lists, like C/C++"
-# B = A.split()
-# for i in B:
-# 	H.insert(i)
 
-print H.get_hash('6')
-print H.get_hash('Lb')
+if __name__ == '__main__':
+	wc = 0
+	with open('testFile.txt', 'r') as f:
+		for line in f:
+			B = line.split()
+			for i in B:
+				wc += 1
+				H.insert(i, -1, wc)
 
-H.insert('6')
-H.insert('Lb')
-H.print_hash()
-H.delete('Lb')
-H.print_hash()
+	print H.find('soon')
+	H.increase('soon')
+	print H.find('soon')
+	H.delete('soon')
+	print H.find('soon')
+
+	H.print_hash()
